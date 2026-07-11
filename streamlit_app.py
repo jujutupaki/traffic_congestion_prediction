a2618a88e50230e37bea328dccb5cb413876c37a
Juliene Baleña <138591836+jujutupaki@users.noreply.github.com>
2026-07-11 21:24:22 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,23 +8,5 @@ st.write('Description!')
 # dataset with CV split
 with st.expander('Traffic_Dataset):
   dataset_df = pd.read_csv('https://raw.githubusercontent.com/jujutupaki/traffic_congestion_prediction/refs/heads/master/Traffic_Dataset.csv')
-  dataset_df['10_Minutes_Interval'] = pd.to_datetime(dataset_df['10_Minutes_Interval'])
-  train_val = dataset_df[(dataset_df['10_Minutes_Interval'] >= pd.Timestamp('2025-01-01 00:00:00')) & (dataset_df['10_Minutes_Interval'] <= '2025-11-23 11:50:00')].reset_index(drop=True)
-  test = dataset_df[(dataset_df['10_Minutes_Interval'] >= pd.Timestamp('2025-11-24 00:00:00'))].reset_index(drop=True)
-  # creating x (selected features only) and y variables
-  selected_features = ['Hour','temperature_2m (°C)',
-                      'soil_temperature_0_to_7cm (°C)',
-                      'Driving Direction',
-                      'apparent_temperature (°C)',
-                      'soil_temperature_7_to_28cm (°C)',
-                      'surface_pressure (hPa)',
-                      'vapour_pressure_deficit (kPa)',
-                      'Minute',
-                      'DayOfYear']
-  X = train_val[selected_features]
-  y = train_val['Simulated Traffic Level']
-  X_test = test[selected_features]
-  y_test = test['Simulated Traffic Level']
-  
-  X
-  X_test
+  dataset_df
+      
