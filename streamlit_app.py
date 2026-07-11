3b5b7e0288e564b4e62e7cffc8f432e413782a3c
Juliene Baleña <138591836+jujutupaki@users.noreply.github.com>
2026-07-11 21:26:54 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,5 @@ st.title('🚗 Traffic Congestion Prediction')
 
 st.write('Description!')
 
-# dataset with CV split
-dataset_df = pd.read_csv('https://raw.githubusercontent.com/jujutupaki/traffic_congestion_prediction/refs/heads/master/Traffic_Dataset.csv')
-dataset_df
+# 
       
