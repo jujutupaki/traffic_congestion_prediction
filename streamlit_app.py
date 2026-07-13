1f838487594f5c549837657eede3bd08321b09cb
Juliene Baleña <138591836+jujutupaki@users.noreply.github.com>
2026-07-13 13:53:40 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,9 +23,9 @@ with st.sidebar:
 dataset_df['10_Minutes_Interval'] = pd.to_datetime(dataset_df['10_Minutes_Interval'])
 train_val = dataset_df[(dataset_df['10_Minutes_Interval'] >= pd.Timestamp('2025-01-01 00:00:00')) & (dataset_df['10_Minutes_Interval'] <= '2025-11-23 11:50:00')].reset_index(drop=True)
 test = dataset_df[(dataset_df['10_Minutes_Interval'] >= pd.Timestamp('2025-11-24 00:00:00'))].reset_index(drop=True)
-X = train_val.drop(columns=['Simulated Traffic Level', '10_Minutes_Interval','Vehicle_Count'])
+X = train_val.drop(columns=['Simulated Traffic Level', '10_Minutes_Interval'])
 y = train_val['Simulated Traffic Level']
-X_test = test.drop(columns=['Simulated Traffic Level', '10_Minutes_Interval','Vehicle_Count'])
+X_test = test.drop(columns=['Simulated Traffic Level', '10_Minutes_Interval'])
 y_test = test['Simulated Traffic Level']
 
 with st.expander():
