e20b2e7b93590648e6655038a5e76626514c8afe
Juliene Baleña <138591836+jujutupaki@users.noreply.github.com>
2026-07-13 13:02:15 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,11 +3,12 @@ import pandas as pd
 
 st.title('🚗 Traffic Congestion Prediction')
 
-st.info('Welcome to brr-traffic.streamlit.app, the interactive dashboard for our thesis: "Predicting Traffic Congestion under Different Weather Conditions Using Machine Learning Approaches".\n
-What you can explore here:\n
-🚦 Dataset Overview: The final traffic-weather data using selected features\n
-📈 Model Evaluation Dashboard: Compare performance metrics across the Random Forest, XGBoost, and LSTM models\n
-🔮 Interactive Traffic Predictor: Input custom weather conditions, dates, and times to generate real-time congestion predictions')
+st.info("""Welcome to brr-traffic.streamlit.app, the interactive dashboard for our thesis: "Predicting Traffic Congestion under Different Weather Conditions Using Machine Learning Approaches".
+
+What you can explore here:
+🚦 **Dataset Overview:** The final traffic-weather data using selected features
+📈 **Model Evaluation Dashboard:** Compare performance metrics across the Random Forest, XGBoost, and LSTM models
+🔮 **Interactive Traffic Predictor:** Input custom weather conditions, dates, and times to generate real-time congestion predictions""")
 
 with st.expander('View Final Traffic-Weather Dataset'):
       dataset_df = pd.read_csv('https://raw.githubusercontent.com/jujutupaki/traffic_congestion_prediction/refs/heads/master/Selected_Features_Traffic_Dataset.csv')
