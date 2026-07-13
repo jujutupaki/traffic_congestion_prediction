3d9ea9d149e004b710a8300972a7e6d94f0843b5
Juliene Baleña <138591836+jujutupaki@users.noreply.github.com>
2026-07-13 13:50:43 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,4 +21,14 @@ with st.sidebar:
 
 #CV SPlit
 dataset_df['10_Minutes_Interval'] = pd.to_datetime(dataset_df['10_Minutes_Interval'])
+train_val = dataset_df[(dataset_df['10_Minutes_Interval'] >= pd.Timestamp('2025-01-01 00:00:00')) & (dataset_df['10_Minutes_Interval'] <= '2025-11-23 11:50:00')].reset_index(drop=True)
+test = dataset_df[(dataset_df['10_Minutes_Interval'] >= pd.Timestamp('2025-11-24 00:00:00'))].reset_index(drop=True)
+X = train_val.drop(columns=['Simulated Traffic Level', '10_Minutes_Interval','Vehicle_Count'])
+y = train_val['Simulated Traffic Level']
+X
+y
 
+X_test = test.drop(columns=['Simulated Traffic Level', '10_Minutes_Interval','Vehicle_Count'])
+y_test = test['Simulated Traffic Level']
+X_test
+y_test
