6b179833e5c229dd68f74312f33485b003f4085b
Juliene Baleña <138591836+jujutupaki@users.noreply.github.com>
2026-07-20 22:37:06 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,10 +25,7 @@ st.set_page_config(
 
 st.title('🚗 Traffic Congestion Prediction')
 
-st.info("""
-🔴 **Dataset Overview:** The final traffic-weather dataset using selected features\n
-🟡 **Model Evaluation Dashboard:** Compare performance metrics across the Random Forest, XGBoost, and LSTM models\n
-🟢 **Traffic Predictor:** Input custom weather conditions, dates, and times to generate real-time congestion predictions\n""")
+st.info("""🔴 **Dataset Overview:** The final traffic-weather dataset using selected features\n🟡 **Model Evaluation Dashboard:** Compare performance metrics across the Random Forest, XGBoost, and LSTM models\n🟢 **Traffic Predictor:** Input custom weather conditions, dates, and times to generate real-time congestion predictions\n""")
 
 #dataset
 dataset_df = pd.read_csv('https://raw.githubusercontent.com/jujutupaki/traffic_congestion_prediction/refs/heads/master/Traffic_Data_Selected_Features.csv')
