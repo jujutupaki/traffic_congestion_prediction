2634927ce0b184b2b6bc51586d4ede8b7670b946
Juliene Baleña <138591836+jujutupaki@users.noreply.github.com>
2026-07-20 22:54:31 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,23 +4,6 @@ from datetime import timedelta, datetime
 import joblib
 import plotly.express as px
 
-# Remove whitespace from the top of the page and sidebar
-st.markdown("""
-        <style>
-               .css-18e3th9 {
-                    padding-top: 0rem;
-                    padding-bottom: 10rem;
-                    padding-left: 5rem;
-                    padding-right: 5rem;
-                }
-               .css-1d391kg {
-                    padding-top: 3.5rem;
-                    padding-right: 1rem;
-                    padding-bottom: 3.5rem;
-                    padding-left: 1rem;
-                }
-        </style>
-        """, unsafe_allow_html=True)
 
 st.set_page_config(
     layout="wide",
