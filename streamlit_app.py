962acf0545028f69b94878f0fec41b8270b68b78
Juliene Baleña <138591836+jujutupaki@users.noreply.github.com>
2026-07-20 20:50:56 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -156,7 +156,7 @@ if st.button("Start Prediction", use_container_width=True):
 if st.session_state.prediction is not None:
     display_prediction(st.session_state.prediction)
 
-metrics_df = pd.read_csv(https://raw.githubusercontent.com/jujutupaki/traffic_congestion_prediction/refs/heads/master/models/metrics_df.csv,
+metrics_df = pd.read_csv('https://raw.githubusercontent.com/jujutupaki/traffic_congestion_prediction/refs/heads/master/models/metrics_df.csv',
              index_col=0)
 
 st.info("Select metrics to display:")
