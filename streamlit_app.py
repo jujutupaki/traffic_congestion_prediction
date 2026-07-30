02350ffe3a9fb242f45c87a48b07e169ac1475d1
Juliene Baleña <138591836+jujutupaki@users.noreply.github.com>
2026-07-31 00:36:37 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -123,18 +123,34 @@ def display_prediction(prediction):
         f"""
         <div style="
             background-color:{bg_color};
-            padding:25px;
+            padding:20px 15px;
             border-radius:12px;
             text-align:center;
             border:2px solid {text_color};
-            margin-bottom:5px;
+            margin-bottom:30px;
+            height:220px;
+            box-sizing:border-box;
+            display:flex;
+            flex-direction:column;
+            justify-content:center;
+            align-items:center;
         ">
-            <h3 style="margin:0; color:{text_color};">
+        
+            <h3 style="
+                margin:0;
+                color:{text_color};
+            ">
                 🚦 Predicted Traffic Congestion:
             </h3>
-            <h1 style="margin-top:10px; color:{text_color};">
+        
+            <h1 style="
+                margin:10px 0 0 0;
+                color:{text_color};
+                text-align:center;
+            ">
                 {prediction_text}
             </h1>
+        
         </div>
         """,
         unsafe_allow_html=True
