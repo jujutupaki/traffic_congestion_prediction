c2328683d5155e9eb5d0617e454e417628e6d24f
Juliene Baleña <138591836+jujutupaki@users.noreply.github.com>
2026-07-31 02:49:19 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,6 @@ st.set_page_config(
 )
 
 st.title('🚗 Traffic Congestion Prediction for Harrison Road, Baguio City')
-st.html("<hr style='border: none; border-top: 1px solid gray; margin-bottom: 5px;'>")
 
 #CV SPlit
 dataset_df['10_Minutes_Interval'] = pd.to_datetime(dataset_df['10_Minutes_Interval'])
