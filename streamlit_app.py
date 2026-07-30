819d704282b5ed6f69814b9679b79fd27d40d31c
Juliene Baleña <138591836+jujutupaki@users.noreply.github.com>
2026-07-31 04:18:15 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -379,12 +379,11 @@ xgb_lstm_df = pd.DataFrame({
 
 with col2:
     st.subheader("Pairwise McNemar's with Bonferroni Correction")
-    st.info("Select a pairwise comparison:")
     cola, colb = st.columns([1, 3])
 
     with cola:
         selected_pair = st.radio(
-            "Pairwise comparison:",
+            "Select a pair:",
             [
                 "Random Forest & XGBoost",
                 "Random Forest & LSTM",
