c4758e5ef93a5188190cf943642a0e2efe933b6c
Juliene Baleña <138591836+jujutupaki@users.noreply.github.com>
2026-07-31 03:15:55 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -317,6 +317,17 @@ with col1:
     st.metric("p-value","<0.001", border=True)
     st.metric("Decision","Significant", border=True)
 
+st.markdown("""
+    <style>
+    [data-testid="stMetricLabel"] {
+        font-size: 17px;
+    }
+    [data-testid="stMetricValue"] {
+        font-size: 23px;
+    }
+    </style>
+    """, unsafe_allow_html=True)
+
 with col2:
     st.subheader ("Pairwise McNemar's with Bonferroni Correction")
     st.metric("New p-value with Bonferroni Correction", "0.0167")
