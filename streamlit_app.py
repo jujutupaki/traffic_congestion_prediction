6c6053bc3db8e1729dbd38d7c8d65df4f1b5bee7
Juliene Baleña <138591836+jujutupaki@users.noreply.github.com>
2026-07-31 02:45:16 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -195,7 +195,7 @@ Dense clustering of vehicles
 Delayed traffic speed
 """)
 
-st.divider()
+st.html("<hr style='border-color: gray;'>")
 st.header("Final Dataset Features & Ranking:", divider="gray")
 
 final_features_df = pd.read_csv(
@@ -220,7 +220,7 @@ features_fig.update_layout(
 
 st.plotly_chart(features_fig)
 
-st.divider()
+st.html("<hr style='border-color: gray;'>")
 st.header("Model Evaluation using Classification Metrics:", divider="gray")
 
 metrics_df = pd.read_csv("https://raw.githubusercontent.com/jujutupaki/traffic_congestion_prediction/refs/heads/master/models/metrics_df.csv",
@@ -294,7 +294,7 @@ with col2:
     else:
         st.warning("Please select at least one model and one metric.")
         
-st.divider()
+st.html("<hr style='border-color: gray;'>")
 st.header("Model Evaluation using Statistical Tools:", divider="gray")
 
 st.subheader ("Cochran's Q Test")
