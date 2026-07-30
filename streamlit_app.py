74cdfa897b78544a55aaaa181e99e2681ce14752
Juliene Baleña <138591836+jujutupaki@users.noreply.github.com>
2026-07-31 01:34:24 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -179,18 +179,18 @@ with col2:
     st.info("""**Legends for interpretation:**
 
 🟢 **Low Traffic**  
-Minimal vehicle volume detected.
-Wide gaps between vehicles.
-Free-flowing movement.
+Minimal vehicle volume detected.  
+Wide gaps between vehicles.  
+Free-flowing movement.  
 
 🟡 **Moderate Traffic**  
-Increased vehicle volume detected.
-Average and steady moving traffic.
-Minor speed reductions.
+Increased vehicle volume detected.  
+Average and steady moving traffic.  
+Minor speed reductions.  
 
 🔴 **Heavy Traffic**  
-Peak vehicle volume detected.
-Dense clustering of vehicles.
+Peak vehicle volume detected.  
+Dense clustering of vehicles.  
 Delayed traffic speed.
 """)
 
