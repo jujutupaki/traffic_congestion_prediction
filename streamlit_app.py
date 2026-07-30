7675f0f85d98f3da6f2d7ec7b90ceb9b4977b345
Juliene Baleña <138591836+jujutupaki@users.noreply.github.com>
2026-07-31 03:08:55 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -306,7 +306,7 @@ st.markdown("""
         font-size: 20px;
     }
     [data-testid="stMetricValue"] {
-        font-size: 35px;
+        font-size: 28px;
     }
     </style>
     """, unsafe_allow_html=True)
