cfbdcfdac972fbef9e686decbeae30e1ee94ea76
Juliene Baleña <138591836+jujutupaki@users.noreply.github.com>
2026-07-31 04:23:46 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -483,7 +483,7 @@ with col2:
                 yaxis_title="Model Advantage",
                 xaxis_title="Model",
                 yaxis=dict(range=[0, 210]),
-                margin=dict(t=3)
+                margin=dict(t=0)
             )
 
             st.plotly_chart(
